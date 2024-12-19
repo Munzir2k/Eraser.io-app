6703296b6e8624f4e66d1f97863953d63d16ef2c
Munzir2k <114055530+Munzir2k@users.noreply.github.com>
2024-12-20 00:57:33 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
-import {withAuth} from '@kinde-oss/kinde-auth-nextjs/middleware';
+import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware';
+
+const authDomain = 'https://munzir.kinde.com'; // Update this to your Kinde Auth instance URL
+
 export default function middleware(req: any) {
   return withAuth(req, {
     isReturnToCurrentPage: true,
+    authDomain, // Add this option to the withAuth config
   });
 }
-export const config = {
-  matcher: ['/dashboard'],
-};
