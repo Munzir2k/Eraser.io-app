c7b50aeb3de7808ae6de1a8a3c1eeca6f483ed37
Mohammad Munzir <mohammadmunzir2k@gmail.com>
2025-01-09 19:32:45 +0530

    Update

--- app/(routes)/dashboard/_components/FileLists.tsx
+++ app/(routes)/dashboard/_components/FileLists.tsx
@@ -1,102 +1,117 @@
-import {FileListContext} from '@/app/_context/FileListContext';
-import React, {useContext, useEffect, useState} from 'react';
-import moment from 'moment';
-import {useKindeBrowserClient} from '@kinde-oss/kinde-auth-nextjs';
-import Image from 'next/image';
-import {Archive, MoreHorizontal} from 'lucide-react';
+/** @format */
+
+import { FileListContext } from "@/app/_context/FileListContext";
+import React, { useContext, useEffect, useState } from "react";
+import moment from "moment";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import Image from "next/image";
+import { Archive, MoreHorizontal } from "lucide-react";
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import {useRouter} from 'next/navigation';
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 
 export interface FILE {
-  archive: boolean;
-  createdBt: string;
-  document: string;
-  fileName: string;
-  teamId: string;
-  whiteboard: string;
-  _id: string;
-  _creationTime: number;
+    archive: boolean;
+    createdBt: string;
+    document: string;
+    fileName: string;
+    teamId: string;
+    whiteboard: string;
+    _id: string;
+    _creationTime: number;
 }
 
 function FileLists() {
-  const {fileList_, setFileList_} = useContext(FileListContext);
-  const [fileList, setFileList] = useState<any>();
-  const {user}: any = useKindeBrowserClient();
-  const router = useRouter();
+    const { fileList_, setFileList_ } = useContext(FileListContext);
+    const [fileList, setFileList] = useState<any>();
+    const { user }: any = useKindeBrowserClient();
+    const router = useRouter();
 
-  useEffect(() => {
-    fileList_ && setFileList(fileList_);
-    console.log(fileList_);
-  }, [fileList_]);
+    useEffect(() => {
+        fileList_ && setFileList(fileList_);
+        console.log(fileList_);
+    }, [fileList_]);
 
-  return (
-    <div className="overflow-x-auto mt-10">
-      <table className="min-w-full divide-y-2 dark:bg-zinc-950 divide-gray-200 bg-white dark:divide-zinc-700 text-sm">
-        <thead className="ltr:text-left rtl:text-right">
-          <tr>
-            <td className="whitespace-nowrap px-4 py-2 font-bold text-gray-900 dark:text-gray-300">
-              File Name
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 font-bold text-gray-900 dark:text-gray-300">
-              Created At
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 font-bold text-gray-900 dark:text-gray-300">
-              Edited
-            </td>
-            <td className="whitespace-nowrap px-4 py-2 font-bold text-gray-900 dark:text-gray-300">
-              Author
-            </td>
-          </tr>
-        </thead>
+    return (
+        <div className="overflow-x-auto mt-10">
+            <table className="min-w-full divide-y-2 dark:bg-zinc-950 divide-gray-200 bg-white dark:divide-zinc-700 text-sm">
+                <thead className="ltr:text-left rtl:text-right">
+                    <tr>
+                        <td className="whitespace-nowrap px-4 py-2 font-bold text-gray-900 dark:text-gray-300">
+                            File Name
+                        </td>
+                        <td className="whitespace-nowrap px-4 py-2 font-bold text-gray-900 dark:text-gray-300">
+                            Created At
+                        </td>
+                        <td className="whitespace-nowrap px-4 py-2 font-bold text-gray-900 dark:text-gray-300">
+                            Edited
+                        </td>
+                        <td className="whitespace-nowrap px-4 py-2 font-bold text-gray-900 dark:text-gray-300">
+                            Author
+                        </td>
+                    </tr>
+                </thead>
 
-        <tbody className="divide-y divide-gray-200 dark:divide-zinc-700">
-          {fileList &&
-            fileList.map((file: FILE, index: number) => (
-              <tr key={index}>
-                <td
-                  onClick={() => router.push(`/workspace/${file._id}`)}
-                  className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-gray-300">
-                  <p className="w-fit cursor-pointer">{file.fileName}</p>
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-300">
-                  {moment(file._creationTime).format('DD MMM YYYY')}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-300"></td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-300">
-                  <Image
-                    className="rounded-full cursor-pointer"
-                    src={user?.picture}
-                    width={40}
-                    height={40}
-                    alt="user"
-                  />
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-300">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger className="outline-none">
-                      <MoreHorizontal />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                      <DropdownMenuItem className="gap-3">
-                        <Archive className="w-5 h-5" /> Archive
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    </div>
-  );
+                <tbody className="divide-y divide-gray-200">
+                    {fileList &&
+                        fileList.map((file: FILE, index: number) => (
+                            <tr
+                                key={index}
+                                className="odd:bg-gray-50 cursor-pointer"
+                                onClick={() =>
+                                    router.push("/workspace/" + file._id)
+                                }
+                            >
+                                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                    {file.fileName}
+                                </td>
+                                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                    {moment(file._creationTime).format(
+                                        "DD MMM YYYY"
+                                    )}{" "}
+                                </td>
+                                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                    {moment(file._creationTime).format(
+                                        "DD MMM YYYY"
+                                    )}
+                                </td>
+                                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                    {user && (
+                                        <Image
+                                            src={user?.picture}
+                                            alt="user"
+                                            width={30}
+                                            height={30}
+                                            className="rounded-full"
+                                        />
+                                    )}
+                                </td>
+                                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger>
+                                            <MoreHorizontal />
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuItem className="gap-3">
+                                                <Archive className="h-4 w-4" />{" "}
+                                                Archive
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default FileLists;
--- app/(routes)/dashboard/layout.tsx
+++ app/(routes)/dashboard/layout.tsx
@@ -1,53 +1,58 @@
-'use client';
-import {api} from '@/convex/_generated/api';
-import {useKindeBrowserClient} from '@kinde-oss/kinde-auth-nextjs';
-import {useConvex} from 'convex/react';
-import {useRouter} from 'next/navigation';
-import React, {useEffect, useState} from 'react';
-import SideNav from './_components/SideNav';
-import {FileListContext} from '@/app/_context/FileListContext';
-import {ThemeProvider} from '@/components/ThemeProvider';
+/** @format */
+
+"use client";
+import { api } from "@/convex/_generated/api";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useConvex } from "convex/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import SideNav from "./_components/SideNav";
+import { FileListContext } from "@/app/_context/FileListContext";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 function DashboardLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  const {user}: any = useKindeBrowserClient();
-  const convex = useConvex();
-  const router = useRouter();
-  const [fileList_, setFileList_] = useState();
+    const { user }: any = useKindeBrowserClient();
+    const convex = useConvex();
+    const router = useRouter();
+    const [fileList_, setFileList_] = useState();
 
-  useEffect(() => {
-    user && checkTeam();
-  }, [user]);
+    useEffect(() => {
+        user && checkTeam();
+    }, [user]);
 
-  const checkTeam = async () => {
-    const result = await convex.query(api.teams.getTeams, {email: user?.email});
+    const checkTeam = async () => {
+        const result = await convex.query(api.teams.getTeams, {
+            email: user?.email,
+        });
 
-    if (!result.length) {
-      router.push('/teams/create');
-    }
-  };
+        if (!result.length) {
+            router.push("/teams/create");
+        }
+    };
 
-  return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      // enableSystem
-      disableTransitionOnChange>
-      <div>
-        <FileListContext.Provider value={{fileList_, setFileList_}}>
-          <div className="grid grid-cols-4">
-            <div className="h-screen w-[20rem] fixed">
-              <SideNav />
+    return (
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            // enableSystem
+            disableTransitionOnChange
+        >
+            <div>
+                <FileListContext.Provider value={{ fileList_, setFileList_ }}>
+                    <div className="grid grid-cols-4">
+                        <div className="h-screen w-[20rem] fixed">
+                            <SideNav />
+                        </div>
+                        <div className="col-span-4 pl-[20rem]">{children}</div>
+                    </div>
+                </FileListContext.Provider>
             </div>
-            <div className="col-span-4 pl-[20rem]">{children}</div>
-          </div>
-        </FileListContext.Provider>
-      </div>
-    </ThemeProvider>
-  );
+        </ThemeProvider>
+    );
 }
 
 export default DashboardLayout;
